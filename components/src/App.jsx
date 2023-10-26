ab157614c810fe00ee8a256f9cdd7ba7022b4cc8
DilyanaStoyanova <dilyanazhstoyanova@gmail.com>
2023-10-26 11:01:53 +0300

    Movie List

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,26 +1,12 @@
 import './App.css';
+import movies from './assets/movies';
 import MovieList from './components/Movielist';
-
-const movies = [
-    {
-        title: 'The Matrix',
-        description: 'Sth here'
-    },
-    {
-        title: 'The Man of Steel',
-        description: 'Sth here'
-    },
-    {
-        title: 'Harry Potter',
-        description: 'Sth here'
-    }
-];
 
 function App() {
     return (
         <>
           <h1>My First Dynamic React Application</h1> 
-          <MovieList headingText="Movie List exclusive" />
+          <MovieList movies={movies} headingText="Movie List exclusive Hype" secondaryHeading="Best of best"/>
         </>
     );
 }
--- components/src/components/Movielist.jsx
+++ components/src/components/Movielist.jsx
@@ -1,13 +1,17 @@
 import Movie from "./Movie";
 
 export default function MovieList(props) {
+    console.log(props)
     return (
         <div className="site-list">
-            <h1>Movie List</h1>
+            <h1>{props.headingText}</h1>
+            <h2>{props.secondaryHeading}</h2>
             <ul>
-                <Movie />
-                <Movie />
-                <Movie />
+                <li>{props.movies[0].title}</li>
+                <li>{props.movies[1].title}</li>
+                <li>{props.movies[2].title}</li>
+                <li>{props.movies[3].title}</li>
+                <li>{props.movies[4].title}</li>
             </ul>
         </div>
     );
